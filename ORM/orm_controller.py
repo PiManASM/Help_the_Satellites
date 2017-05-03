25e6d6b3f925d989018f08b344a39496d423efdb
williamstrong <william.strong@me.com>
2017-05-02 20:24:46 -0700

    Simplified ORM file. Created code to commit DB. Start query code.
    Need DB tables finalized to finish code and create the DB.

--- ORM/orm_controller.py
+++ ORM/orm_controller.py
@@ -2,31 +2,67 @@ from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import create_engine
 from sqlalchemy.exc import InvalidRequestError
+from sqlalchemy import Column, Integer, String
+
 
 # class creates the connect on instantiation and contains methods for updating data to the database as well as pulling
 # data from the database
+
+Base = declarative_base()
+engine = create_engine('mysql+mysqlconnector://root:root@localhost/satellite', echo=True)
+
+
+class Identification(Base):
+
+    __tablename__ = 'identification'
+
+    cospar_desg = Column(String(6), primary_key=True)
+    norad_id = Column(String(10))  # primary_key=True)
+    name = Column(String(64))
+    alt_name = Column(String(64))
+    julian_date = Column(String(64))
+    gregorian_date = Column(String(64))
+    geo_stat = Column(String(64))
+    orbital_period = Column(Integer())
+    perigee = Column(Integer())
+    apogee = Column(Integer())
+    inclination = Column(Integer())
+    longitude = Column(String(64))
+    drift_rate = Column(String(64))
+
+
 class Controller:
 
-    engine = create_engine('mysql+mysqlconnector://root:root@localhost/satellite', echo=True)
-
-    Base = declarative_base()
-
     session_create = sessionmaker()
-    session_create.configure(bind=Base)
+    session_create.configure(bind=engine)
     session = session_create()
 
-    def nothing_exist(self):
-        # use the meta data to create tables.
-        # this should probably raise an error. It will indicate some messed up stuff
-        pass
+    # def nothing_exist(self):
+    #     # use the meta data to create tables.
+    #     # this should probably raise an error. It will indicate some messed up stuff
+    #     pass
 
-    def update_data(self):
-        pass
+    # def update_data(self):
+    #     pass
 
-    def add_data(self, add_map):
+    # def add_data(self, add_map):
+    #
+    #     try:
+    #         self.session.add_all(add_map)
+    #         self.session.commit()
+    #     except InvalidRequestError:
+    #         self.session.rollback()
+    #         raise
+    #     except: #I cannot remember the name of the error, but the one that happens when nothing exist
+    #         # call nothing_exist and make stuff
+    #         pass
+
+    # the important class
+    def query_data(self):  # , query):
         try:
-            self.session.addall(add_map)
-            self.session.commit()
-        except InvalidRequestError:
-            self.session.roll_back()
-            raise
+            for satellite in self.session.query(Identification.cospar_desg).\
+                    filter_by(norad_id='1986-096A'):
+                print(satellite)
+        except: # some exception
+            pass
+        return satellite
--- get_data.py
+++ get_data.py
@@ -1,33 +1,68 @@
-import csv
-from ORM.orm import Identification
+# import csv
+from ORM.orm_controller import Controller, Identification
 
-class Data_Retr:
+
+# class DataRetr:
+#     def __init__(self):
+#         self.data_list = []
+#
+#     def web(self, file):
+#         pass
+#
+#     def file(self, file):
+#         # read the file
+#         with open(file) as csvfile: # 'data/geo.csv'
+#             data = csv.reader(csvfile, delimiter=',')
+#             for row in data:
+#                 self.data_list.append(row)
+#         return self.create_obj()
+#
+#     def save_data(self, data):
+#         pass
+#
+#     def create_obj(self):
+#         # create data objects to be put into the db via the session
+#         session_instances = []
+#         for new_row in self.data_list:
+#             new_user = Identification(cospar_desg=new_row[0],
+#                                       norad_id=new_row[1],
+#                                       name=new_row[2],
+#                                       alt_name=new_row[3],
+#                                       julian_date=new_row[4],
+#                                       gregorian_date=new_row[5],
+#                                       geo_stat=new_row[6],
+#                                       orbital_period=new_row[7],
+#                                       perigee=new_row[8],
+#                                       apogee=new_row[9],
+#                                       inclination=new_row[10],
+#                                       longitude=new_row[11],
+#                                       drift_rate=new_row[12]
+#                                       )
+#
+#             session_instances.append(new_user)
+#         return session_instances
+
+
+class Data:
     def __init__(self):
+        self.control = Controller()
+
+    # def data_add(self, data):
+    #     self.control.add_data(data)
+
+    def data_query(self, query):
         pass
 
-    def web(self, file):
-        pass
 
-    def file(self, file):
-        # read the file
-        data_list = []
-        with open(file) as csvfile: # 'data/geo.csv'
-            data = csv.reader(csvfile, delimiter=',')
-            for row in data:
-                data_list.append(row)
+if __name__ == "__main__":
+    # add to DB
+    # new_file = DataRetr()
+    # sats = new_file.file('data/geo.csv')
+    # add = Controller()
+    # add.add_data(sats)
 
-    def save_data(self, data):
-        pass
-
-    def create_obj(self, data):
-        # create data objects to be put into the db via the session
-        session_instances = []
-        for row_new in data:
-            new_user = Identification(row_new)
-            session_instances.append(new_user)
-
-
-        pass
-
-if __name__ == '__main__':
-    pass
+    # query DB
+    query = Controller()
+    data = query.query_data()
+    print(type(data))
+    print(data[0])
